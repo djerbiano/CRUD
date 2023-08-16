3297b298071acccc84240054e4b0f4d39c69599e
djerbiano <saberghoudi2222@hotmail.fr>
2023-08-16 21:44:53 +0200

    update project

--- app.js
+++ app.js
@@ -5,19 +5,31 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 
+// Middleware pour enregistrer les informations de la requête
+const logger = (req, res, next) => {
+  console.log(
+    `Date : ${new Date().toISOString()} method: ${req.method} URL: ${req.path}`
+  );
+  next();
+};
+app.use(logger);
+
+// Route pour la page d'accueil
 app.get("/", (req, res) => {
-  console.log(`${req.method}${req.path}`);
   res.status(200).send("<h1>Welcom to the home page</h1>");
 });
 
+// Routes pour interagir avec les articles (posts)
 app.get("/posts", controller.getAll);
 app.get("/posts/:id", controller.getOne);
+app.post("/posts", controller.addOne);
 app.delete("/posts/:id", controller.deletOne);
 app.patch("/posts/:id", controller.updateOne);
 
+// Route pour gérer les erreurs 404 (page non trouvée)
 app.get("*", (req, res) => {
-  console.log(`${req.method}${req.path}`);
   res.status(404).send("<h1>Page not found</h1>");
 });
 
+// Démarrer le serveur
 app.listen(port, () => console.log(`listen server on port ${port}`));
--- controllers/posts.js
+++ controllers/posts.js
@@ -6,19 +6,37 @@ const controller = {
   },
 
   getOne: (req, res) => {
+    // Trouver le post correspondant à l'ID dans les paramètres de la requête
     const foundPost = posts.find((post) => post.id === parseInt(req.params.id));
 
     if (foundPost) {
+      // Renvoyer le post trouvé si existant
       res.status(200).json(foundPost);
     } else {
+      // Renvoyer une erreur 404 si le post n'existe pas
       res.status(404).send("Post doesn't exist");
     }
   },
 
+  addOne: (req, res) => {
+    // Créer un nouveau post à partir des données de la requête
+    const post = {
+      userId: req.body.userId,
+      id: posts[posts.length - 1].id + 1,
+      title: req.body.title,
+      body: req.body.body,
+    };
+    // Ajouter le nouveau post au tableau
+    posts.push(post);
+    res.status(200).send("post is added");
+  },
+
   deletOne: (req, res) => {
+    // Trouver le post correspondant à l'ID dans les paramètres de la requête
     const foundPost = posts.find((post) => post.id === parseInt(req.params.id));
 
     if (foundPost) {
+      // Trouver l'index du post et le supprimer du tableau
       const index = posts.indexOf(foundPost);
 
       posts.splice(index, 1);
@@ -30,9 +48,11 @@ const controller = {
   },
 
   updateOne: (req, res) => {
+    // Trouver le post correspondant à l'ID dans les paramètres de la requête
     const foundPost = posts.find((post) => post.id === parseInt(req.params.id));
 
     if (foundPost) {
+      // Mettre à jour les propriétés du post avec les nouvelles données de la requête
       foundPost.userId = req.body.userId || foundPost.userId;
       foundPost.id = req.body.id || foundPost.id;
       foundPost.title = req.body.title || foundPost.title;
